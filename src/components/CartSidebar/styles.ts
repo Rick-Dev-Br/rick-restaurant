4391528340876270b5039fa9a0f5aabc5518345a
Rick-Dev-Br <henrique_rickgamer@hotmail.com>
2025-09-30 12:50:15 -0300

    fix placeholder

--- src/components/CartSidebar/styles.ts
+++ src/components/CartSidebar/styles.ts
@@ -21,6 +21,12 @@ export const SidebarContainer = styled.div`
   z-index: 1000;
   box-shadow: -2px 0 10px rgba(0, 0, 0, 0.1);
 
+  input::placeholder,
+  textarea::placeholder {
+    color: ${cores.textosP};
+    opacity: 1;
+  }
+
   @media (max-width: 768px) {
     width: 100%;
   }
